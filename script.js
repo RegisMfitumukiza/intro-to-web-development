c917f26caa56eb4b2f34999930ad3dbbffbff563
Regis <mfitumukizaregis90@gmail.com>
2025-06-14 14:56:41 +0200

    Linked Navbar Menu Items to Their Sections and Displayed the Current Year in the Footer

--- script.js
+++ script.js
@@ -3,10 +3,24 @@
 function changeBackgroundColor(color){
     let section = document.getElementById('coding-journey');
     section.style.backgroundColor = color;
-}
+};
 
 document.getElementById('intro').addEventListener('click', () => changeBackgroundColor("#2CA02C"));
 
 document.getElementById('react').addEventListener('click', () => changeBackgroundColor("#FF7F0E"));
 
 document.getElementById('backend').addEventListener('click', () => changeBackgroundColor("#9467BD"));
+
+
+function moveToSection(sectionId) {
+    let section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
+};
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const yearElement = document.getElementById('currentYear');
+    const newDate = new Date().getFullYear();
+    yearElement.textContent = newDate;
+});
+
